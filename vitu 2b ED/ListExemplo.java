e4b0379ef8ce13da8e623c032fcd4fb04b3b1e9d
VictorEhzz <161712445+VictorEhzz@users.noreply.github.com>
2024-05-06 11:14:37 -0300

    estrutura de dados victor emanoel hilario 2b

---
+++ vitu 2b ED/ListExemplo.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class ListExemplo{
+
+
+    public static void main(String[] args){
+
+        List<String> nomeAlunoList = new ArrayList();
+
+        nomeAlunoList.add("João");
+        nomeAlunoList.add("Carlos");
+        nomeAlunoList.add("Jeff");
+        nomeAlunoList.add("Kara");
+        nomeAlunoList.add("Mary");
+        nomeAlunoList.add("Jenny");
+        Collections.sort(nomeAlunoList);
+
+        System.out.println("Essa é sua lista" + nomeAlunoList);
+        System.out.println("Abaixo estão os elementos pego na posição");
+        System.out.println(nomeAlunoList.get(4));
+        System.out.println(nomeAlunoList.remove(1));
+        System.out.println("O tamanho da lista é: " + nomeAlunoList.size());
+        System.out.println("A lista está assim agora" + nomeAlunoList);
+    }
+}
